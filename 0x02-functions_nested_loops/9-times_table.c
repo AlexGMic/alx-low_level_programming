232638f4bd055254063fe94e9725d48b71af180c
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 14:15:47 -0400

    project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,6 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (j == 9)
-{
 product = i * j;
 if (product / 10 > 0)
 {
@@ -25,22 +23,10 @@ else
 _putchar(' ');
 }
 _putchar(product % 10 + '0');
-}
-else
+if (j < 9)
 {
-product = i * j;
-if (product / 10 > 0)
-{
-_putchar(product / 10 + '0');
-}
-else
-{
-_putchar(' ');
-}
-_putchar(product % 10 + '0');
 _putchar(',');
 _putchar(' ');
-
 }
 }
 _putchar('\n');
