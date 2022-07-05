458f130403277222b2702e96c8878767828e5e7d
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 16:20:34 -0400

    project

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int n)
 {
 while (n < 98)
 {
-printf("%d",n);
+printf("%d, ",n);
 n++;
 }
 while (n > 98)
 {
-printf("%d",n);
+printf("%d, ",n);
 n--;
 }
-printf("%d",98);
-printf('\n');
+printf("98");
+_putchar('\n');
 }
