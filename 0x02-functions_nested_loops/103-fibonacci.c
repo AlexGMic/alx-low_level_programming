59e52f00911bfc39d5154adda9b61596450f6dff
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 18:18:50 -0400

    project

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print the sum of even Fibonacci numbers 
+ * Description: Print the sum of even Fibonacci numbers
  * Return: 0
  */
 int main(void)
