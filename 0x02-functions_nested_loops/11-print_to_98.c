999339a26b09bd466b25b44d9115ef2488d2da51
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 16:18:26 -0400

    project

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_to_98 - prints series of numbers starting from n to 98
+*@n: is the starting point
+*Return: Always 0
+*/
+
+void print_to_98(int n)
+{
+while (n < 98)
+{
+printf("%d",n);
+n++;
+}
+while (n > 98)
+{
+printf("%d",n);
+n--;
+}
+printf("%d",98);
+printf('\n');
+}
