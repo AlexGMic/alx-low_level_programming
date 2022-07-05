2481fc4775779dee176477659028772ea4071c65
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 10:47:47 -0400

    project

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,4 @@
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
