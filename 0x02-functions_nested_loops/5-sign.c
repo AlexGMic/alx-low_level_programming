5dd5c0fa808f1482834dbe74c2566a007f5e1432
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 11:39:23 -0400

    project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 
 /**
 *print_sign: print the sign for the respective number
-*@n: int type
+*@n: integer type can be positive, negative or zero
 *Description: print +, 0, or - based on the number
 *Return: 1 if +, 0 if 0, and -1 if -
 */
 
 int print_sign(int n)
 {
-if (n>0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (n==0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
