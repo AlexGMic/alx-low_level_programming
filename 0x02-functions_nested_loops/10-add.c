77db4ed5b3f1d72809b68f3f46950058b46457fb
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 14:25:46 -0400

    project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - returns the computed sum
+*@a: int to be added
+*@b: int to be added
+*Return: integer
+*/
+
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
