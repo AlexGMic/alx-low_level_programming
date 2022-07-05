bb2633b2b644818d85616be67f2cef07acd40a82
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 11:27:01 -0400

    project

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
-*Checks if the given input is an alphabet
+*_isalpha - checks if character is alphabet or not
+*@c: type int char
+*Return: 1 if letter, lowercase or uppercase
 */
 
 int _isalpha(int c)
 {
-if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
 {
 return (1);
 }
@@ -14,19 +16,4 @@ else
 {
 return (0);
 }
-}
-int main(void)
-{
-int r;
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
 }
