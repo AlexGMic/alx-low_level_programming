9253253a12602c8eaff5b98efc1d7d517395fa98
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 14:21:07 -0400

    project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,20 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 product = i * j;
-if (product / 10 > 0)
+if (j == 0)
 {
-_putchar(product / 10 + '0');
+_putchar(product + '0');
+}
+else if (product < 10)
+{
+_putchar(' ');
+_putchar(product + '0');
 }
 else
 {
-_putchar(' ');
-}
+_putchar(product / 10 + '0');
 _putchar(product % 10 + '0');
+}
 if (j < 9)
 {
 _putchar(',');
