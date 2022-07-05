cfdf1ccb05094837c105c75293e40b70ea1d43bf
AlexGMic <alemkgbremichael@gmail.com>
2022-07-05 12:05:19 -0400

    project

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*_abs - converts a negative number to positive using an absoulte value
-*@i: int type number
-*Return - absolute value of @i
+* _abs - return abs value
+* @i: int type number
+* Return - absolute value of @i
 */
 
 int _abs(int i)
