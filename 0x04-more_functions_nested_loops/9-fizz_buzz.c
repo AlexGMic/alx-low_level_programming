33fdc774409f97b964e9de56e529454f44af84ac
AlexGMic <alemkgbremichael@gmail.com>
2022-07-07 08:41:58 -0400

    loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main - print fizz in place of 3 and buzz in place of 5
-*
-*Return: Always 0
-*/
+ * main - prints 1 through 100 and replace 3 by Fizz and 5 by Buzz
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
 }
-else if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-}
-else if (i % 3 != 0 && i % 5 == 0)
-{
-printf(" Buzz");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else
-{
-printf(" %d ", i);
-}
-}
-printf("\n");
-return (0);
+	putchar('\n');
+	return (0);
+
 }
