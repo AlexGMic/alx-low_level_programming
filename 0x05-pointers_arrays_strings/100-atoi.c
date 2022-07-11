e1a41ef9b99971bedd30e1870e4a786cfeab3856
AlexGMic <alemkgbremichael@gmail.com>
2022-07-11 13:17:25 -0400

    array

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- *_atoi - converts a string to an integer.
- *@s: input string.
- *Return: integer.
- */
+*_atoi - converts a string to an integer.
+*@s: input string.
+*Return: integer.
+*/
 int _atoi(char *s)
 {
 unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
