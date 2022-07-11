426c209a560c436a1f339938d06b334b7fcd87bb
AlexGMic <alemkgbremichael@gmail.com>
2022-07-11 07:34:21 -0400

    array

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_rev - prints the string in reverse order.
+*@s: character pointer.
+*
+*Return: Always 0.
+*/
+void print_rev(char *s)
+{
+int len = 0;
+while (*s != 0)
+{
+s++;
+len++;
+}
+int i;
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(*s[i]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void _putchar(char);
+void print_rev(char *s);
