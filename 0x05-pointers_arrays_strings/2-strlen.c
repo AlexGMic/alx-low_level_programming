e77eebe8983ab7e413f73d405eb0f025045089a1
AlexGMic <alemkgbremichael@gmail.com>
2022-07-11 06:39:52 -0400

    array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strlen - counts the length of a string.
+*@s: character pointer.
+*
+*Return: returns an int type length of a string. 
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+s++;
+length++;
+}
+return (length);
+}
