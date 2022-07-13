2e41d81504687814a424361a642c3d14c1115b86
AlexGMic <alemkgbremichael@gmail.com>
2022-07-13 09:09:35 -0400

    array

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_strncat - concatenates the string by copying specified number of characters.
+*@dest: is the destination character pointer.
+*@src: is the source character pointer.
+*@n: number of characters to be copied.
+*
+*Return: returns the destination string pointer.
+*/
+void *_strncat(char *dest, char *src, int n)
+{
+int i, j, len1, len2;
+len1 = 0;
+len2 = 0;
+j = 0;
+while (dest[len1] != '\0')
+{
+len1++;
+}
+while (src[len2] != '\0')
+{
+len2++;
+}
+for (i = len1; i < len1 + n; i++)
+{
+if (n <= len2)
+{
+dest[i] = src[j];
+j++;
+}
+}
+return (dest);
+}
