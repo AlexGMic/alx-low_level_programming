bd12ad0354eb21c4c5d8efc77a87ce21ac125691
AlexGMic <alemkgbremichael@gmail.com>
2022-07-13 13:54:14 -0400

    array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*reverse_array - reverses the order of a given array.
+*@a: is an integer pointer.
+*@n: is the length of the array.
+*/
+void reverse_array(int *a, int n)
+{
+int i, mid, high, temp;
+i = 0;
+high = n - 1;
+mid = n / 2;
+while(mid--)
+{
+temp = a[i];
+a[i] = a[high - i];
+a[high - i] = temp;
+i++; 
+}
+print_array(a, n);
+}
