fe81928f791f915e7d6f57044cef6f508c46504c
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 15:46:30 -0400

    array

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,16 +8,22 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i, j;
-for (i = 0; *(haystack + i) != '\0'; i++)
-{
-for (j = 0; *(needle + j) != '\0'; j++)
-{
-if (*(haystack + i) == *(needle + j))
-{
-return (haystack + i);
-}
-}
-}
-return ('\0')
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
 }
