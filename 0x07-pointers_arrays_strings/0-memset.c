1bb3410282bae2868a924447fa5cf24f45b213d3
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 12:29:32 -0400

    array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_memset - is a function that returns a pointer after coping a values to it.
+*@s: is a character pointer.
+*@b: is a character variable.
+*@n: is an integer variable.
+*
+*Return: the function returns pointer.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (s[i] != '\0')
+{
+s[i] = b;
+}
+}
+return (s);
+}
