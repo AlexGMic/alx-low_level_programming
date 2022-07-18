0f90b5003f2766c7be0267716fac24b84d1aaa16
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 14:06:02 -0400

    array

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int i, j, len = 0;
 for (i = 0; s[i] >= '\0'; i++)
 {
-for (j = 0; accept[j] >= '\0'; j++)
+for (j = 0; accept[j] > '\0'; j++)
 {
 if (s[i] == accept[j])
 {
