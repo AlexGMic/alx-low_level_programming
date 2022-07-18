5b7171c294bedbc37ec6a3dc3ee4df017abcda68
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 13:29:44 -0400

    array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strchr - returns the character after the first detected letter.
+*@s: a character pointer.
+*@c: a character variable.
+*
+*Return: returns a pointer to the first character occurrence.
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (*(s + i) == c)
+{
+return (s + i);
+}
+}
+return ('\0');
+}
