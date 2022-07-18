c3d555219a0db9794e5b8ca9c14b6936336a359c
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 13:48:07 -0400

    array

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strspn - returns the number of bytes consisting of the bytes in accept.
+*@s: is the checking character string.
+*@accept: is a character string to be checked.
+*
+*Return: returns an unsigned int type.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, len;
+len = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+len++;
+}
+}
+}
+return (len);
+}
