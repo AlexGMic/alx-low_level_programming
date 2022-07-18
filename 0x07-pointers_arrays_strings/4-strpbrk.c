2ffd57ef746d30a22a339731623c951d3cb20bad
AlexGMic <alemkgbremichael@gmail.com>
2022-07-18 15:25:43 -0400

    array

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,13 +9,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 unsigned int i, j;
-for (i = 0; accept[i] != '\0'; i++)
+for (i = 0; *(s + i) != '\0'; i++)
 {
-for (j = 0; s[j] != '\0'; j++)
+for (j = 0; *(accept + j) != '\0'; j++)
 {
-if (*(s + j) == *(accept + i))
+if (*(s + i) == *(accept + j))
 {
-return (s + j);
+return (s + i);
 }
 }
 }
