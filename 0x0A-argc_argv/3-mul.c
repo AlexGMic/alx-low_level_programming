e5bf3d5783d1452287ff9fc842b1dae6c3bd6727
AlexGMic <alemkgbremichael@gmail.com>
2022-07-22 05:30:12 -0400

    arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the product of two numbers.
+*@argc: int type that counts the arguments in the command line.
+*@argv: contains or holds the command line arguments.
+*
+*Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+int i;
+int result = 1;
+if (argc != 3)
+{
+printf("Error");
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+result *= atoi(argv[i]);
+}
+printf("%d\n", result);
+}
+return (0);
+}
