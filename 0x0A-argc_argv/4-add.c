46627249952bdebdbe77bf8f7c2f30078a1de7ba
AlexGMic <alemkgbremichael@gmail.com>
2022-07-22 05:43:22 -0400

    arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints the sum of positive numbers.
+*@argc: int type that counts the arguments in the command line.
+*@argv: contains or holds the command line arguments.
+*
+*Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+int i;
+int result = 0;
+for (i = 1; i < argc; i++)
+{
+if (0 <= atoi(argv[i] <=9)
+{
+result += atoi(argv[i]);
+printf("%d\n", result);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+return (0);
+}
