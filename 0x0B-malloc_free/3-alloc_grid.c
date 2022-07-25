000fe915c08ef3c795ed5905aaf04b98b13f5835
AlexGMic <alemkgbremichael@gmail.com>
2022-07-25 12:18:06 -0400

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*alloc_grid - forms two dimensional array of input width and height.
+*@width: int type.
+*@height: int type.
+*
+*Return: integer pointer on success.
+*/
+int **alloc_grid(int width, int height)
+{
+int **ptr, i, j;
+if (width < 1 || height < 1)
+{
+return (NULL);
+}
+ptr = (int *)malloc(sizeof(int) * (height + width + 1));
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+for (i = 0; i < width; i++)
+{
+for (j = 0; j < height; j++)
+{
+ptr[i][j] = 0;
+}
+}
+return (ptr);
+}
