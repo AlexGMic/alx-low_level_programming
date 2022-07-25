ccbdb3dd5a8778accc2c9ee162815c8ec92b9aba
AlexGMic <alemkgbremichael@gmail.com>
2022-07-25 09:53:30 -0400

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strdup - returns the duplicate of the input string.
+*@str: character pointer.
+*
+*Return: returns a character pointer.
+*/
+char *_strdup(char *str)
+{
+unsigned int size, i;
+char *ptr;
+size = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[size] != '\0')
+{
+size++;
+}
+ptr = (char *) malloc((size + 1) * (sizeof(char));
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+ptr[i] = str[i];
+}
+return (ptr);
+}
