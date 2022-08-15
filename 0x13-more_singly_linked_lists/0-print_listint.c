ae1de0d11a01ff2c964ede77cc7128547e3670bf
AlexGMic <alemkgbremichael@gmail.com>
2022-08-15 15:46:21 -0400

    single

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,18 +10,11 @@ size_t print_listint(const listint_t *h)
 size_t count = 0;
 const listint_t *ptr = NULL;
 ptr = h;
-if (h == NULL)
-{
-printf("Empty list");
-}
-else
-{
 while (ptr != NULL)
 {
 printf("%d\n", ptr->n);
 ptr = ptr->next;
 count++;
-}
 }
 return (count);
 }
